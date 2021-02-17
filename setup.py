7c073ff8e2e4d6f484c66ff27cd88a865d911a59
a1k89 <akoptev1989@ya.ru>
2021-02-17 19:37:50 +0300

    change setup

--- setup.py
+++ setup.py
@@ -27,5 +27,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.0',
+    python_requires='>=3.8',
 )
