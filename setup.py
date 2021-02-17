0328ae89b94554b8893e9e829c1ede9cfa2a2cff
a1k89 <akoptev1989@ya.ru>
2021-02-17 17:19:16 +0300

    bug fix: relative path

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     keywords='django',
     url="https://github.com/a1k89/django-rest-sms-auth",
-    packages=setuptools.find_packages('smsauth'),
+    packages=setuptools.find_packages(),
     install_requires=['Django >= 3.0',
                       'djangorestframework',
                       'django-phonenumber-field',
